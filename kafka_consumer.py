badd1825ecf90bc4817263f92bbad90ee510bc56
ztsl1234 <45007601+ztsl1234@users.noreply.github.com>
2025-11-18 01:00:45 +0900

    Update kafka_consumer.py

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,15 +1,69 @@
-from kafka import KafkaConsumer
-import json
 
-consumer = KafkaConsumer(
-    "risk_trades",
-    bootstrap_servers="localhost:9092",
-    auto_offset_reset="earliest",
-    enable_auto_commit=True,
-    value_deserializer=lambda v: json.loads(v.decode("utf-8"))
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import (
+    from_json, col, window
+)
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
+
+spark = SparkSession.builder \
+    .appName("RiskTradesStreamingJob") \
+    .config("spark.sql.shuffle.partitions", "200") \
+    .getOrCreate()
+
+spark.sparkContext.setLogLevel("WARN")
+
+trade_schema = StructSchema = StructType([
+    StructField("trade_id", StringType()),
+    StructField("counterparty", StringType()),
+    StructField("notional", DoubleType()),
+    StructField("currency", StringType()),
+    StructField("timestamp", TimestampType())
+])
+
+raw_df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "risk_trades") \
+    .option("startingOffsets", "latest") \
+    .load()
+
+# Kafka value is bytes → cast to string
+json_df = raw_df.selectExpr("CAST(value AS STRING) as json_str")
+
+parsed_df = json_df.select(
+    from_json(col("json_str"), trade_schema).alias("data")
+).select("data.*")
+
+windowed_agg = (
+    parsed_df
+    .withWatermark("timestamp", "10 minutes")
+    .groupBy(
+        window(col("timestamp"), "5 minutes"),
+        col("currency")
+    )
+    .sum("notional")
+    .withColumnRenamed("sum(notional)", "total_notional")
 )
 
-print("Listening for messages...\n")
+alert_stream = parsed_df.filter(
+    (col("notional") > 2_000_000) & (col("currency") == "USD")
+)
 
-for msg in consumer:
-    print("Received:", msg.value)
+agg_query = (
+    windowed_agg.writeStream
+    .format("delta")
+    .outputMode("append")
+    .option("checkpointLocation", "/delta/checkpoints/risk_window_agg")
+    .start("/delta/risk_window_agg")
+)
+
+alert_query = (
+    alert_stream.writeStream
+    .format("delta")     
+    .outputMode("append")
+    .option("checkpointLocation", "/delta/checkpoints/alerts")
+    .start("/delta/alerts")
+
+
+agg_query.awaitTermination()
+alert_query.awaitTermination()
